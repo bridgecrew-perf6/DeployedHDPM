32ca3dd264cb7fbc07673f69f580cb5a09f03843
Mohamed Hossam <mhossamem@gmail.com>
2020-11-26 13:42:01 +0200

    added DB URL

--- config/db.js
+++ config/db.js
@@ -1,2 +1,2 @@
-export const database = 'mongodb://localhost:27017/HospWebAppDB';
+export const database = 'mongodb+srv://mhossamem:HospWebApp1000@hospwebappcluster.ge8bq.mongodb.net/HospWebAppDB?retryWrites=true&w=majority';
 export const secret = 'yoursecret';
