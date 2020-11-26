af2109a856aa41fe12f33f869619210653ef0031
Mohamed Hossam <mhossamem@gmail.com>
2020-11-26 16:07:52 +0200

    password for user changed

--- config/db.js
+++ config/db.js
@@ -1,2 +1,2 @@
-export const database = 'mongodb+srv://mhossamem:HospWebApp1000@hospwebappcluster.ge8bq.mongodb.net/HospWebAppDB?retryWrites=true&w=majority';
+export const database = 'mongodb+srv://mhossamem:Deph1420@hospwebappcluster.ge8bq.mongodb.net/HospWebAppDB?retryWrites=true&w=majority';
 export const secret = 'yoursecret';
