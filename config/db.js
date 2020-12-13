bfad61cef998b3a64459381f3ddd30866703d67b
Mohamed Hossam <58301677+mhossamem@users.noreply.github.com>
2020-12-13 22:49:03 +0200

    Update db.js

--- config/db.js
+++ config/db.js
@@ -1,2 +1,2 @@
-export const database = 'mongodb+srv://mhossamem:Deph1420@hospwebappcluster.ge8bq.mongodb.net/HospWebAppDB?retryWrites=true&w=majority';
+export const database = '<DB URL>'; //Insert DB URL here
 export const secret = 'yoursecret';
